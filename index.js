0f942135952606da2b5a0e6683d9e7b019b4ac77
Stur Shi <dongjie.shi@ichezheng.com>
2017-05-23 21:02:43 +0800

    babel env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from './src/app.js';
 
 const env = process.env.NODE_ENV || 'development';
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 6003;
 app.listen(port);
